5e5922d920d92a54cf1b055add3c26a85c716a2e
Amira207 <30653652+Amira207@users.noreply.github.com>
2017-09-03 11:39:59 +0200

    Update classic.js

--- js/classic.js
+++ js/classic.js
@@ -3,38 +3,46 @@
 
 $(document).ready(function () {
     "use strict";
-   
-    
+
+
     //adjust header height
     $(".header").height($(window).height());
     $(window).resize(function () {
         $(".header").height($(window).height());
-         //adjust bx slider list item center
+        //adjust bx slider list item center
         $(".bxslider").each(function () {
             $(this).css("paddingTop", ($(window).height() - $(".bxslider li").height()) / 2);
         });
     });
-    
+
     //add active class
-    $(".links li").click(function () {
-        $(this).addClass("active").siblings().removeClass("active");
-    });
+    /* $(".links li").click(function () {
+         $(this).addClass("active").siblings().removeClass("active");
+     });
+     $(".links2 div").click(function () {
+         $(this).addClass("active").siblings().removeClass("active");
+     });*/
     //adjust bx slider list item center
     $(".bxslider").each(function () {
         $(this).css("paddingTop", ($(window).height() - $(".bxslider li").height()) / 2);
     });
-    
+
     //trigger the bx slider
     $('.bxslider').bxSlider({
-        pager : false
+        pager: false
     });
-    
+
     $(".links li a ").click(function () {
         $("html, body").animate({
             scrollTop: $("#" + $(this).data("value")).offset().top
         }, 1000);
     });
-    
+    $(".links2 a").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#" + $(this).data("value")).offset().top
+        }, 1000);
+    });
+
     (function autoSlider() {
         $(".slider .active").each(function () {
             if (!$(this).is(":last-child")) {
@@ -51,15 +59,25 @@ $(document).ready(function () {
             }
         });
     }());
-    
-  //trigger mix it up
+
+    //trigger mix it up
     $("#container").mixItUp();
     //adjust selected li
     $(".projects li").click(function () {
         $(this).addClass("selected").siblings().removeClass("selected");
     });
-    
-   $("html").niceScroll({
-       zIndex: 9999999
-   });
+
+    //$("html").niceScroll({
+    // zIndex: 9999999
+    // });
+    $(".fa-bars").on("click", function () {
+            $(".links2").toggleClass("fading");
+            $(".bx-next").fadeToggle();
+            $(".bx-wrapper").toggleClass("fading1");
+        }
+
+    );
+
+    //click bars
+
 });
